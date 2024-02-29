208eee7b952d99668601811f727a2c21ed1dd602
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2024-02-29 21:47:23 +0100

    responsive design on components ok

--- src/components/BannerImgAbout.js
+++ src/components/BannerImgAbout.js
@@ -1,10 +1,32 @@
-import bgImg from '../assets/img_about.png'
+import React, { useState, useEffect } from 'react'
 
 function BannerImgAbout() {
+    const [isScreenWider, setIsScreenWider] = useState(window.innerWidth > 480)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsScreenWider(window.innerWidth > 480)
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
     return (
-        <div className="BannerImgAbout-container">
-            <div className="BannerImgAbout-img"></div>
-        </div>
+        <>
+            {isScreenWider ? (
+                <div className="BannerImgAbout-container">
+                    <div className="BannerImgAbout-img"></div>
+                </div>
+            ) : (
+                <div className="BannerImgAbout-container">
+                    <div className="BannerImgAbout-img"></div>
+                </div>
+            )}
+        </>
     )
 }
 
--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,27 +1,66 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-function Collapse({ str , text }) {
+function Collapse({ str, text }) {
     const [windowDown, setWindowDown] = useState(false)
+    const [isScreenWider, setIsScreenWider] = useState(window.innerWidth > 480)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsScreenWider(window.innerWidth > 480)
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
 
     return (
         <div className="container">
-            <div className="collapse-container">
-                <div>{str}</div>
-                <div onClick={() => setWindowDown(!windowDown)}>
-                    <FontAwesomeIcon
-                        icon={windowDown ? faChevronUp : faChevronDown}
-                    />
-                </div>
-            </div>
-            <div className={`collapse-window ${windowDown && 'window-down'}`}>
-                <div
-                    className={`window-content ${windowDown ? 'd-block' : 'd-none'}`}
-                >
-                    <p>{text}</p>
-                </div>
-            </div>
+            {isScreenWider ? (
+                <>
+                    <div className="collapse-container">
+                        <div>{str}</div>
+                        <div onClick={() => setWindowDown(!windowDown)}>
+                            <FontAwesomeIcon
+                                icon={windowDown ? faChevronUp : faChevronDown}
+                            />
+                        </div>
+                    </div>
+                    <div
+                        className={`collapse-window ${windowDown && 'window-down'}`}
+                    >
+                        <div
+                            className={`window-content ${windowDown ? 'd-block' : 'd-none'}`}
+                        >
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className="collapse-container">
+                        <div>{str}</div>
+                        <div onClick={() => setWindowDown(!windowDown)}>
+                            <FontAwesomeIcon
+                                icon={windowDown ? faChevronUp : faChevronDown}
+                            />
+                        </div>
+                    </div>
+                    <div
+                        className={`collapse-window ${windowDown && 'window-down'}`}
+                    >
+                        <div
+                            className={`window-content ${windowDown ? 'd-block' : 'd-none'}`}
+                        >
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
